b95ec5b6f732f3297bf048d193b79fdf89f48b96
Avery Nguyen <averynguyen@Averys-MacBook-Air.local>
2021-08-16 18:27:14 -0600

    added hover effect to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './styles/App.scss'
+
 
 function App() {
   return (
-    <div>
-      Photo Site
+    <div className='app'>
+      <div className='about'>
+        <div>About</div>
+      </div>
+      <div className='stills'>
+        Photo Site
+      </div>
+      <div className='contact'>
+        Contact
+      </div>
     </div>
   );
 }
