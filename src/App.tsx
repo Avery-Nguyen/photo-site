3c8e9aa34bf001c93e4c81d576d4ab65189b6dff
Avery Nguyen <averynguyen@Averys-MacBook-Air.local>
2021-08-17 17:34:20 -0600

    added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/App.scss'
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from './components/About';
+import Stills from './components/Stills';
+import Contact from './components/Contact';
 
 function App() {
+  const [aboutPage, setAboutPage] = useState(false)
+
   return (
-    <div className='app'>
-      <div className='about'>
-        <div>About</div>
+  <Router>
+      <div className='app'>
+        <div className='about'>
+          <Link to="/about">About</Link>
+        </div>
+        <div className='stills'>
+        <Link to="/stills">Stills</Link>
+        </div>
+        <div className='contact'>
+        <Link to="/contact">Contact</Link>
+        </div>
       </div>
-      <div className='stills'>
-        Photo Site
-      </div>
-      <div className='contact'>
-        Contact
-      </div>
-    </div>
+      <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/stills">
+            <Stills />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+  </Router>
   );
 }
 
